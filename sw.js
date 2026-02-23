65085d459c83254cdd4acbd0ab8b94f8b20a3bdb
Steve Meskens <steve.meskens@gmail.com>
2026-02-23 22:48:21 +0100

    Initial commit

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "padel-service-v1";
+const ASSETS = ["./","./index.html","./manifest.json","./sw.js","./icon-192.png","./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cached) => cached || fetch(event.request)));
+
+    
+});
